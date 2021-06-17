8c62befe35c8eff1fc43bef482fbeb7e97f39b80
denisRun <polozov.denis77@gmail.com>
2021-06-17 19:24:30 +0300

    Добавление сбора, проверка на ввод данных

--- scripts/SelectChats.js
+++ scripts/SelectChats.js
@@ -105,9 +105,8 @@ function SelectHouses(isHouseKeeper,modalWindowParams, localStorageParams, ShowM
 
     //добавляем дома
     // k == idДома
-    console.log(housesData);
     console.log(housesData.chats.apartment_chat_chat['0']['chat_id']);
-    /*for(var k in housesData.houses)
+    for(var k in housesData.chats.apartment_chat_chat)
     {
         var houseElement = document.createElement("div");
 
@@ -124,137 +123,57 @@ function SelectHouses(isHouseKeeper,modalWindowParams, localStorageParams, ShowM
             this.style.backgroundColor = "rgba(255,164,51,0.73)";
         };
 
-        houseElement.id = "houseElement" + k;
-        houseElement.name = k;
+        houseElement.id = "houseElement" + housesData.chats.apartment_chat_chat[k]['chat_id'];
+        houseElement.name =housesData.chats.apartment_chat_chat[k]['chat_id'];
         houseElement.onclick = function()
         {
-            localStorage.setItem(localStorageParams.HouseId, this.name);
-            localStorage.setItem(localStorageParams.HouseName, this.children[0].innerHTML);
+            localStorage.setItem(localStorageParams.ChatID, this.name);
             DeleteElement(houseElement.parentElement.id);
-            switch (isHouseKeeper) {
-                case true:
-                    HouseKeeperGetPorches(isHouseKeeper,this.name, modalWindowParams, localStorageParams, ShowModalFunction );
-                    break;
-                case false:
-                    AdminGetPorches(isHouseKeeper,houseId, modalWindowParams, localStorageParams, ShowModalFunction);
-                    break;
-            }
+            ShowModalFunction(); 
         };
 
         var houseName = document.createElement("h4");
         houseName.style.textAlign = "left";
         houseName.style.paddingTop = (parseInt(houseElement.style.height) / 3 ) + "px";
         houseName.style.alignSelf = "center";
-        houseName.innerHTML = housesData.houses[k].house_number;
+        houseName.innerHTML = housesData.chats.apartment_chat_chat[k]['chat_id'];
 
         houseElement.appendChild(houseName);
         houseContainer.appendChild(houseElement);
-    }*/
-}
-
-function AdminGetPorches(isHouseKeeper,houseId, modalWindowParams, localStorageParams, ShowModalFunction)
-{
-
-}
-
-// Запрашиваем подъезды по указанному Id дома
-function HouseKeeperGetPorches(isHouseKeeper,houseId, modalWindowParams, localStorageParams, ShowModalFunction)
-{
-    const head =
-        {
-            'Content-type': 'application/json; charset=UTF-8',
-            'Authorization': 'Bearer ' + localStorage.getItem(localStorageParams.HouseKeeperToken)
-        };
-
-    fetch('https://prp-project-server.herokuapp.com/api/porch/' + houseId, {
-        method: 'GET',
-        headers: head,
-    })
-        .then(response => response.json())
-        .then(Data => {
-            console.log('Success:', Data);
-            porchesData = Data;
-            SelectPorches(modalWindowParams, localStorageParams, ShowModalFunction);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-}
-
-//Добавляем выбор подъездов в диалоговом окне
-function SelectPorches(modalWindowParams, localStorageParams, ShowModalFunction )
-{
-    modal = document.getElementById(modalWindowParams.modalBodyDivId);
-
-    // добавляем область для подъездов
-    var porchContainer = document.createElement("div");
-    porchContainer.className = "container-fluid  p-0 m-0";
-    porchContainer.id = "porchContainer0";
-
-    modal.appendChild(porchContainer);
-
-    //добавляем надпись
-    document.getElementById(modalWindowParams.modalHeaderTitleId).innerHTML = "Оберіть необхідний під`їзд";
-
-    //добавляем кнопку для создания новости
-    var porchCreateButton = document.createElement("button");
-    porchCreateButton.id = "porchCreateButton";
-    porchCreateButton.innerHTML = "Додати новину до " + localStorage.getItem(localStorageParams.HouseName);
-    porchCreateButton.className = "btn btnAdd mb-4 ";
-    porchCreateButton.style.fontSize = "20px";
-    porchCreateButton.onclick = function()
+    }
+    for(var k in housesData.chats.porch_chat)
     {
-        localStorage.setItem(localStorageParams.PorchId, "null");
-        localStorage.setItem(localStorageParams.PorchName, "null");
-        DeleteElement(porchContainer.id);
-        //ShowModalEditor();
-        ShowModalFunction();        // Используем переданую функцию для далнейшей роботы программы
-    };
-    porchContainer.appendChild(porchCreateButton);
+        var houseElement = document.createElement("div");
 
-    var porchDivsContainer = document.createElement("div");
-    porchDivsContainer.className = "container-fluid list-group p-0 m-0";
-    porchDivsContainer.id = "porchDivsContainer0";
-
-    //добавляем подъезды
-    //k == News_id
-    let firstElem = true;
-    for(var k in porchesData.porches)
-    {
-        var porchElement = document.createElement("div");
-        porchElement.className = "list-group-item list-group-item-action border-2 pt-0";
-        porchElement.style.height = "100px";
-        porchElement.style.maxWidth = "795px";
-        porchElement.style.backgroundColor = "rgba(255,198,98,0.73)";
-        porchElement.style.borderColor = "rgba(0,0,0,0.73)";
-        porchElement.onmouseover = function(){
-            this.style.backgroundColor = "rgba(205,154,81,0.73)";
+        houseElement.className = "list-group-item list-group-item-action border-2 pt-0";
+        houseElement.style.minHeight = "100px";
+        houseElement.style.height = "100px";
+        houseElement.style.maxWidth = "795px";
+        houseElement.style.backgroundColor = "rgba(255,164,51,0.73)";
+        houseElement.style.borderColor = "rgba(0,0,0,0.73)";
+        houseElement.onmouseover = function(){
+            this.style.backgroundColor = "rgba(201,134,38,0.73)";
         };
-        porchElement.onmouseleave = function(){
-            this.style.backgroundColor = "rgba(255,198,98,0.73)";
+        houseElement.onmouseleave = function(){
+            this.style.backgroundColor = "rgba(255,164,51,0.73)";
         };
 
-
-        porchElement.id = "porchElement" + k;
-        porchElement.name = k;
-        porchElement.onclick = function()
-        {
-            localStorage.setItem(localStorageParams.PorchId, this.name);
-            localStorage.setItem(localStorageParams.PorchName, this.children[0].innerHTML);
-            DeleteElement(porchContainer.id);
-            //ShowModalEditor();
-            ShowModalFunction();    // Используем переданую функцию для далнейшей роботы программы
+        houseElement.id = "houseElement" + housesData.chats.porch_chat[k]['chat_id'];
+        houseElement.name = housesData.chats.porch_chat[k]['chat_id'];
+        houseElement.onclick = function()
+        {            console.log(this.name + "baba")
+            localStorage.setItem(localStorageParams.ChatID, this.name);
+            DeleteElement(houseElement.parentElement.id);
+            ShowModalFunction(); 
         };
 
+        var houseName = document.createElement("h4");
+        houseName.style.textAlign = "left";
+        houseName.style.paddingTop = (parseInt(houseElement.style.height) / 3 ) + "px";
+        houseName.style.alignSelf = "center";
+        houseName.innerHTML =housesData.chats.porch_chat[k]['chat_id'];
 
-        var porchName = document.createElement("h4");
-        porchName.style.textAlign = "left";
-        porchName.style.paddingTop = (parseInt(porchElement.style.height) / 3 ) + "px";
-        porchName.style.alignSelf = "center";
-        porchName.innerHTML = porchesData.porches[k].porch_name;
-
-        porchElement.appendChild(porchName);
-        porchDivsContainer.appendChild(porchElement);
-        porchContainer.appendChild(porchDivsContainer);
+        houseElement.appendChild(houseName);
+        houseContainer.appendChild(houseElement);
     }
 }
