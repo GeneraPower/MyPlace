465447eb6d1f0d8e188cec4dedd16b837798813d
denisRun <polozov.denis77@gmail.com>
2021-06-18 03:40:42 +0300

    q

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static(__dirname + '/static'))
+app.use(express.static(__dirname + '/Static'))
 app.use(express.static(__dirname + '/CSS'))
 app.use(express.static(__dirname + '/scripts'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/static/index.html')
+  res.sendFile(__dirname + '/Static/index.html')
 })
 
 app.listen(process.env.PORT)
